18bfff2d0499dbfcd02d6ee728e9c3f6cc93d6c3
JPREmbDev <121400656+JPREmbDev@users.noreply.github.com>
2025-02-26 20:22:08 +0100

    First test servo

---
+++ include/classServo.h
@@ -0,0 +1,33 @@
+#ifndef SERVO_H__
+#define SERVO_H__
+
+#include <stdint.h>
+
+
+class Servo {
+
+    public:
+
+        Servo();
+
+        /** @brief initialize LEDC, call it only once. */
+        void initHw();
+        
+        /** @brief Set the min and max 'us' for the servo control 
+         *  @param min [in] Minimum us for the servo
+         *  @param max [in] Maximum us for the servo
+        */
+        void calibrate(uint32_t min, uint32_t max);
+
+        /** @brief Position in percentage                   
+         *  @param pos [in] Position in percentage 100% = max, 0% = min 
+         */
+        void setPos(uint32_t pos);
+
+    private:
+
+
+};
+
+
+#endif // SERVO_H__ 
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 
 #include "classBmi160.h"
+#include "classServo.h"
+#include "esp_log.h"
 
 #include <freertos/FreeRTOS.h>
 
@@ -7,10 +9,29 @@ extern "C" void app_main();
 
 
 Bmi160 imu;
+Servo servo;
 
 void app_main() {
 
     vTaskDelay(pdMS_TO_TICKS(1000));
+
+    servo.initHw();
+    uint32_t angle = 20;
+    for(;;)
+    {
+        ESP_LOGE("MAIN", "Angle: %lu", angle);
+        servo.setPos(angle);
+        angle ++;
+        if (angle > 100)
+        {
+            angle = 20;
+        }
+        vTaskDelay(pdMS_TO_TICKS(50));
+
+    }
+
+
+    return;
 
     Bmi160SpiConfig config = {
         .spiHost = SPI3_HOST,
